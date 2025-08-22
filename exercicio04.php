164aab8c4331faa5b7c8d7bfd051b8918da63ae2
helenacani2 <you@example.com>
2025-08-22 12:54:43 -0300

    Exercício 4 - incompleta

---
+++ exercicio04.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 04 - Fatorial</title>
+</head>
+<body>
+    <form method="POST" action=""> <!--post ENVIA dados, get BUSCA dados-->
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="fatorial">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if(isset($_POST['fatorial'])){
+            $numero = $_POST['numero'];
+            $array = [];
+
+            for ($i = $numero; $i <= 1; $i--) {
+                
+
+                
+
+            }
+        }
+
+    }
+
+    ?>
+</body>
+</html>
