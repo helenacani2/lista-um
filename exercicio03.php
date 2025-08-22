62b4be73d926ddf920af5ee76854273d9072cc3e
helenacani2 <you@example.com>
2025-08-22 12:48:35 -0300

    Exercício 3

---
+++ exercicio03.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03 -  Informar se um número é positivo, negativo ou zero</title>
+</head>
+<body>
+    <form method="POST" action=""> <!--post ENVIA dados, get BUSCA dados-->
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="sinal">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if(isset($_POST['sinal'])){
+            $numero = $_POST['numero'];
+
+            if ($numero == 0){
+                echo "O número informado é <strong>0</strong>.";
+            } 
+            else if ($numero < 0){
+                echo "O número $numero é <strong>negativo</strong>."; 
+            }  
+            else if ($numero > 0){
+                echo "O número $numero é <strong>positivo</strong>.";
+                }
+            }
+        }
+    ?>
+</body>
+</html>
