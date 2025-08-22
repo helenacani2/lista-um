e544fced8c5895c877a32ea96df9247feec04d5d
helenacani2 <you@example.com>
2025-08-22 12:02:13 -0300

    teste

---
+++ exercicio01.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01 - Par ou Ímpar</title>
+</head>
+<body>
+    <form method="POST" action=""> <!--post ENVIA dados, get BUSCA dados-->
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_par_impar">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if(isset($_POST['verificar_par_impar'])){
+            $numero = $_POST['numero'];
+
+            if ($numero % 2 == 0){
+                echo "O número $numero é <strong>par</strong>.";
+            } else {
+                echo "O número $numero é <strong>ímpar</strong>.";
+            }
+        }
+
+    }
+
+    ?>
+</body>
+</html>
