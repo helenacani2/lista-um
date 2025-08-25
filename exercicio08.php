474d8f701d365b355447e6839a22d3683dbe7927
helenacani2 <you@example.com>
2025-08-25 09:33:45 -0300

    Exercício 8

---
+++ exercicio08.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 8 - Números pares</title>
+</head>
+
+<body>
+    <form method="POST" action="">
+        <label for="numero1">Digite o primeiro número:</label>
+        <input type="number" id="numero1" name="numero1" required>
+
+        <br>
+
+        <button type="submit" name="pares">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['pares'])) {
+            $numero1 = $_POST['numero1'];
+
+            $contador = 0;
+
+            for ($i = 0; $i < $numero1; $i++) {
+                if ($numero1 % 2 == 0) {
+                    $contador += 1;
+                }
+                $numero1--;
+            }
+
+            echo "O número tem <strong>$contador pares</strong> <br>";
+        }
+    }
+    ?>
+</body>
+
+</html>
