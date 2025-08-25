9cf1f9d9924b8147184984aaffcbe4767155b81e
helenacani2 <you@example.com>
2025-08-25 08:51:29 -0300

    Exercício 7

---
+++ exercicio07.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7 - Número perfeito</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="numero1">Digite o primeiro número:</label>
+        <input type="number" id="numero1" name="numero1" required>
+
+        <br>
+          
+        <button type="submit" name="perfeito">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['perfeito'])) {
+            $numero1 = $_POST['numero1'];
+
+            $divisor1 = [];
+            
+                for ($i = 1; $i < $numero1; $i++) {
+                    if ($numero1 % $i == 0) {
+                        $divisor1[] = $i;
+                    }
+                }
+
+            $soma1 = array_sum($divisor1);
+
+        }
+
+        if ($numero1 == $soma1){
+            echo "O números é <strong>PERFEITO!</strong> <br>";
+        } else {
+            echo "O números <strong>NÃO é PERFEITO!</strong> <br>";
+        }
+
+    }
+    ?>
+</body>
+</html>
