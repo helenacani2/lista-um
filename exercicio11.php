8fd543be0dcb2a72c700574a085927fa80f01cf8
helenacani2 <helena_cani@estudante.sesisenai.org.br>
2025-08-26 19:30:48 -0300

    Exercício 11

---
+++ exercicio11.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 11 - Palíndromo </title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="texto">Digite uma palavra:</label>
+        <input type="text" id="texto" name="texto" required>
+
+        <br>
+        
+        <button type="submit" name="palindromo">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['palindromo'])) {
+            $texto = $_POST['texto'];
+
+            $texto_invertido = strrev($texto);
+
+                if ($texto == $texto_invertido) {
+                    echo "$texto <strong>é um palíndromo!</strong> <br>";
+
+                } else {
+                    echo "$texto <strong>NÃO é um palíndromo!</strong> <br>";;
+                }
+            }
+        }
+    ?>
+</body>
+</html>
